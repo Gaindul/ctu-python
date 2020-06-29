a54bd8c84d109a66d103367d020c566c7ed66652
MarkBarwell <MarkBarwell@users.noreply.github.com>
2020-06-30 00:55:58 +0100

    Created Array change script.

---
+++ Homework/Week 2/ArrayChanging.py
@@ -0,0 +1,11 @@
+names=["Jacob", "James", "Anthony", "Megan", "Anna", "John", "Nathan", "Isabelle"]
+removedNames=[]
+names.sort()
+popped=names.pop()
+removedNames.append(popped)
+popped=names.pop()
+removedNames.append(popped)
+popped=names.pop()
+removedNames.append(popped)
+print(names)
+print(removedNames)
---
+++ Homework/Week 2/FixingCode.py
@@ -0,0 +1,20 @@
+color1 = input("Enter the first color: ")
+color2 = input("Enter the second color: ")
+color3 = input("Enter the third color: ")
+color4 = input("Enter the fourth color: ")
+
+colors = ()
+colors.append(color1)
+colors.append(color2)
+colors.append(color3)
+colors.append(color4)
+
+print(colors.length) 
+if colors.length = 4:
+  colors.append("Black")
+  colors.pop()
+
+if color[0] == "Red":
+  print("The first color you thought of was red!")
+else if color[0] == "Green":
+  print("Eww, your first choice was green?!")
