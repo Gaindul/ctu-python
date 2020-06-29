2d3df7f1273dea5dc88089a2fbfb1c196a4caa04
MarkBarwell <MarkBarwell@users.noreply.github.com>
2020-06-29 16:21:26 +0100

    Updated to work

--- Homework/Week1/Listing1.py
+++ Homework/Week1/Listing1.py
@@ -1,6 +1,11 @@
 hourly_wage=int(input("Enter your wage per hour:"))
 hours_worked=int(input("Enter hours worked:"))
 tax_to_take_out=2000
-grosspay=hourly_wage∗hours_worked
-netpay=grosspay−tax_to_take_out
-print("In a typical week,I get paid" hourly_wage "per hour and work for" + hours_worked + "hoursaweek.Igetpaid"+grosspay,"aweek,but get"+tax_to_take_out)
+grosspay=hourly_wage*hours_worked
+netpay=grosspay-tax_to_take_out
+strgrosspay=str(grosspay)
+strnetpay=str(netpay)
+strhours=str(hours_worked)
+strwage=str(hourly_wage)
+strtax=str(tax_to_take_out)
+print("In a typical week,I get paid "+strwage+" per hour and work for "+strhours+" hours a week.I get paid "+strgrosspay+" a week, but get "+strtax)
