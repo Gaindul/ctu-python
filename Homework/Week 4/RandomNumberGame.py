47d2a9dd8ec0e8f8b23ffd832aa530e3058d414e
MarkBarwell <MarkBarwell@users.noreply.github.com>
2020-06-30 16:21:52 +0100

    Created Random Number Game

---
+++ Homework/Week 4/RandomNumberGame.py
@@ -0,0 +1,17 @@
+from random import *
+
+RandomInt=randint(1,15)
+RemainingTries=5
+print(RandomInt)
+while RemainingTries>0:
+    print("Remaining Tries: ", RemainingTries)
+    Guess=int(input("Enter your guess: "))
+    GuessDifference=abs(RandomInt-Guess)
+    if Guess==RandomInt:
+        print("You Win!!")
+        exit()
+    elif GuessDifference<3:
+        print("Your Guess is close")
+    RemainingTries-=1
+if RemainingTries==0:
+    print("You lost. The number was ", RandomInt)
