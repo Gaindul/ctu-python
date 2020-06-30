a024b6676f4dc1ba780616caaf63470671929edc
MarkBarwell <MarkBarwell@users.noreply.github.com>
2020-06-30 01:03:13 +0100

    Fixed errors with the 'Fix Code' challenge.

--- Homework/Week 2/FixingCode.py
+++ Homework/Week 2/FixingCode.py
@@ -3,18 +3,17 @@ color2 = input("Enter the second color: ")
 color3 = input("Enter the third color: ")
 color4 = input("Enter the fourth color: ")
 
-colors = ()
+colors = []
 colors.append(color1)
 colors.append(color2)
 colors.append(color3)
 colors.append(color4)
 
-print(colors.length) 
-if colors.length = 4:
-  colors.append("Black")
-  colors.pop()
-
-if color[0] == "Red":
-  print("The first color you thought of was red!")
-else if color[0] == "Green":
-  print("Eww, your first choice was green?!")
+print(len(colors)) 
+if len(colors)==4:
+    colors.append("Black")
+    colors.pop()
+if colors[0]=="Red":
+    print("The first color you thought of was red!")
+elif colors[0]=="Green":
+    print("Eww, your first choice was green?!")
